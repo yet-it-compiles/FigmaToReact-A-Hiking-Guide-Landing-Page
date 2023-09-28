6ec6bd1808a6e1efb9cdc7c2dbd4ad7e43c722a0
yet-it-compiles <tyler.unsworth@cwu.edu>
2023-09-27 21:39:55 -0700

    🎉 Initialize Project Structure

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+/**
+ * @module App.jsx
+ *  
+ * @description This file is responsible for 
+ *  
+ * @exports App
+ */
+
+/**
+ * @component App
+ *
+ * @description 
+ *
+ * @returns {JSX.Element}
+ */
+const App = () => {
+  return (
+    <>
+      <h1>App Page</h1>
+    </>
+  );
+};
+export default App;
---
+++ src/main.jsx
@@ -0,0 +1,18 @@
+/**
+ * @module App.jsx
+ *  
+ * @description This file is responsible for 
+ *  
+ * @exports App
+ */
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+
+import App from './App.jsx'
+
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+)
